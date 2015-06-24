fb7411ecc4518ec2fb8663bb3b013680f98bcd90
Jonathan Rose <jonr87ee@gmail.com>
2015-06-24 18:16:03 -0400

    Added ability to create a person

--- VoterSim.rb
+++ VoterSim.rb
@@ -1,10 +1,11 @@
 #VoterSim.rb
 
 class Person
-	attr_accessor :name
+	attr_accessor :name, :view
 	
-	def initialize(name)
-		@name = name
+	def initialize(name, view)
+		@name = name 
+		@view = view
 	end
 
 end
@@ -18,10 +19,83 @@ class Politcian < Person
 
 end
 
-# time_to_vote=false
+def unknown_response
+	puts "I'm sorry. I didn't get that. Press enter to try again"
+	gets
+end
 
-# until time_to_vote
+def create
+	puts `clear`
+	valid_response = false
+	puts "What would you like to create? Politician or Person"
+	choice = gets.chomp.downcase
+	case choice
+	when "person"
+		puts `clear`
+		puts "What is the person's name?"
+		name = gets.chomp.downcase.capitalize
+		valid_response = false
+		options = ["liberal", "conservative", "tea party", "socialist", "neutral"]
+		until valid_response
+			puts `clear`
+			puts "What is their political view? Liberal, Conservative, Tea Party, Socialist, or Neutral"
+			view = gets.chomp.downcase
+			if !options.include? view
+				unknown_response
+			else
+				valid_response = true
+			end
+		end
+		Person.new(name,view)
+	when "politician"
+
+	else
+		create
+	end
+	
+end
 
 
+def list
 
-# end
+end
+
+def update
+
+end
+
+def vote
+
+end
+
+
+time_to_vote=false
+people = []
+
+until time_to_vote
+	puts `clear`
+	puts "What would you like to do? Create, List, Update, or Vote"
+	choice = gets.chomp.downcase
+
+	case choice
+	when "create"
+		people << create
+	when "list"
+		list
+	when "update"
+		update
+	when "vote"
+		vote
+		time_to_vote = true
+	else
+		unknown_response
+	end
+
+end
+
+def test(people)
+	p people
+end
+test (people)
+
+
